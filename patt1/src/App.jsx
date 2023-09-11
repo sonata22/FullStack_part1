bd68f09e36decf4f547e0a9ede43dfb12c86cc9e
sonata22 <nataliia.sosnovshchenko@gmail.com>
2023-09-11 14:39:29 +0300

    Task 1.6 done

--- patt1/src/App.jsx
+++ patt1/src/App.jsx
@@ -1,64 +1,38 @@
-const Header = (props) => {
+import { useState } from 'react'
+
+const Button = (props) => {
   return (
-    <>
-      <h1>{props.course}</h1>
-    </>
+    <button onClick={props.action}>{props.text}</button>
   )
 }
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
-
-const Content = (parts) => {
+const Stats = (props) => {
   return (
     <>
-      <Part name={parts.parts[0].name} exercises={parts.parts[0].exercises} />
-      <Part name={parts.parts[1].name} exercises={parts.parts[1].exercises} />
-      <Part name={parts.parts[2].name} exercises={parts.parts[2].exercises} />
-    </>
-  )
-}
-
-const Total = (parts) => {
-  const total = parts.parts[0].exercises + parts.parts[1].exercises + parts.parts[2].exercises
-  return (
-    <>
-      <p>
-        Total number of exercises: {total}
-      </p>
+      <p>good {props.props.good}</p>
+      <p>neutral {props.props.neutral}</p>
+      <p>bad {props.props.bad}</p>
     </>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using parts to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const clickOnGood = () => setGood(good + 1)
+  const clickOnNeutral = () => setNeutral(neutral + 1)
+  const clickOnBad = () => setBad(bad + 1)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>give feedback</h1>
+      <Button action={clickOnGood} text='good' />
+      <Button action={clickOnNeutral} text='neutral' />
+      <Button action={clickOnBad} text='bad' />
+      <Stats props={{ good, neutral, bad }} />
     </div>
   )
 }
